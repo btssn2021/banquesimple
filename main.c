bebb078b6a3257dfdf01d32e833e205a261c95ae
eric <eric.thomas@formation-industries-fc.fr>
2021-10-01 15:27:15 +0200

    depot

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
 int main() {
+    float solde;
     int choix;
+    float depot;
     debut: //étiquette de debut de programme
-    printf("1-choix");
+    printf("\n1-depot\n");
+    printf("2-retrait\n");
     scanf("%d",&choix);
+    if(choix==1)
+    {
+        printf("Combien voulez-vous deposez?\n");
+        scanf("%f",&depot);
+        solde+=depot; //obligatoire
+        printf("Nouveau solde %0.2f",solde);
+    }
+    else
+    {
+        //retrait
+    }
     goto  debut; //retourne à la ligne 5
+
+
     return 0;
 }
